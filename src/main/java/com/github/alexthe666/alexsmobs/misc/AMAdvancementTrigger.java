391421cc953843ff5134b5ba48e52e31759eef43
gerry <geraldhofbauer9@gmail.com>
2025-11-08 21:07:18 +0100

    Fix AMSoundRegistry and AMAdvancementTrigger for 1.21
    - Fixed DeferredHolder<SoundEvent> -> DeferredHolder<SoundEvent, SoundEvent> (200+ sound events)
    - Migrated AMAdvancementTrigger to new Codec-based criterion trigger API
    - Removed JSON serialization methods, added Codec with RecordCodecBuilder
    - Changed to record-based TriggerInstance
    - Network and trigger systems now fully 1.21 compliant

--- src/main/java/com/github/alexthe666/alexsmobs/misc/AMAdvancementTrigger.java
+++ src/main/java/com/github/alexthe666/alexsmobs/misc/AMAdvancementTrigger.java
@@ -1,48 +1,36 @@
 package com.github.alexthe666.alexsmobs.misc;
 
-import com.google.gson.JsonObject;
-import net.minecraft.advancements.critereon.*;
-import net.minecraft.resources.ResourceLocation;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.advancements.Criterion;
+import net.minecraft.advancements.critereon.ContextAwarePredicate;
+import net.minecraft.advancements.critereon.EntityPredicate;
+import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.ExtraCodecs;
 
-public class AMAdvancementTrigger extends SimpleCriterionTrigger<AMAdvancementTrigger.Instance> {
-    public final ResourceLocation resourceLocation;
+import java.util.Optional;
 
-    public AMAdvancementTrigger(ResourceLocation resourceLocation) {
-        this.resourceLocation = resourceLocation;
-    }
-
-    public AMAdvancementTrigger.Instance createInstance(JsonObject p_230241_1_, ContextAwarePredicate p_230241_2_, DeserializationContext p_230241_3_) {
-        return new AMAdvancementTrigger.Instance(p_230241_2_, resourceLocation);
-    }
-
-    public void trigger(ServerPlayer p_192180_1_) {
-        this.trigger(p_192180_1_, (p_226308_1_) -> {
-            return true;
-        });
-    }
+public class AMAdvancementTrigger extends SimpleCriterionTrigger<AMAdvancementTrigger.TriggerInstance> {
 
     @Override
-    public ResourceLocation getId() {
-        return resourceLocation;
+    public Codec<TriggerInstance> codec() {
+        return TriggerInstance.CODEC;
     }
 
+    public void trigger(ServerPlayer player) {
+        this.trigger(player, (instance) -> true);
+    }
 
-    public static class Instance extends AbstractCriterionTriggerInstance {
+    public record TriggerInstance(Optional<ContextAwarePredicate> player) implements SimpleInstance {
+        public static final Codec<TriggerInstance> CODEC = RecordCodecBuilder.create(
+                instance -> instance.group(
+                        ExtraCodecs.strictOptionalField(EntityPredicate.ADVANCEMENT_CODEC, "player").forGetter(TriggerInstance::player)
+                ).apply(instance, TriggerInstance::new)
+        );
 
-        public Instance(ContextAwarePredicate p_i231507_1_, ResourceLocation res) {
-            super(res, p_i231507_1_);
-        }
-
-        public static ConstructBeaconTrigger.TriggerInstance forLevel(MinMaxBounds.Ints p_203912_0_) {
-            return new ConstructBeaconTrigger.TriggerInstance(ContextAwarePredicate.ANY, p_203912_0_);
-        }
-
-
-
-        public JsonObject serializeToJson(SerializationContext p_230240_1_) {
-            JsonObject lvt_2_1_ = super.serializeToJson(p_230240_1_);
-            return lvt_2_1_;
+        public static Criterion<TriggerInstance> instance() {
+            return new Criterion<>(new TriggerInstance(Optional.empty()));
         }
     }
 }
