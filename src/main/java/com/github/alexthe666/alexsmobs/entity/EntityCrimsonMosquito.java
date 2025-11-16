5921afdfea361a32d8a213deb4635013f6c8e147
gerry <geraldhofbauer9@gmail.com>
2025-11-16 19:11:45 +0100

    Fix ColorParticleOption - use create() factory method instead of private constructor

--- src/main/java/com/github/alexthe666/alexsmobs/entity/EntityCrimsonMosquito.java
+++ src/main/java/com/github/alexthe666/alexsmobs/entity/EntityCrimsonMosquito.java
@@ -116,9 +116,8 @@ public class EntityCrimsonMosquito extends Monster {
         this.setMosquitoScale(0.2F);
         this.setFromFly(true);
         for (int j = 0; j < 4; ++j) {
-            // ColorParticleOption takes ParticleType and packed int color (ARGB)
-            int color = 0xFF800000; // Red color
-            this.level().addParticle(new net.minecraft.core.particles.ColorParticleOption(net.minecraft.core.particles.ParticleTypes.ENTITY_EFFECT, color), this.getX() + this.random.nextDouble() / 2.0D, this.getY(0.5D), this.getZ() + this.random.nextDouble() / 2.0D, this.random.nextDouble() * 0.5F + 0.5F, 0.0D, 0.0D);
+            // ColorParticleOption.fromRGB takes RGB float values (0-1)
+            this.level().addParticle(net.minecraft.core.particles.ColorParticleOption.create(net.minecraft.core.particles.ParticleTypes.ENTITY_EFFECT, 0.5F, 0.0F, 0.0F), this.getX() + this.random.nextDouble() / 2.0D, this.getY(0.5D), this.getZ() + this.random.nextDouble() / 2.0D, this.random.nextDouble() * 0.5F + 0.5F, 0.0D, 0.0D);
         }
     }
 
